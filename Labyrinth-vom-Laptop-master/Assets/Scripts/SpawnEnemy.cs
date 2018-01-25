3c1400b5e50b512c45831542a1d72768ee05be88
JERRITE <35013411+JERRITE@users.noreply.github.com>
2018-01-25 08:16:25 +0100

    ?

---
+++ Labyrinth-vom-Laptop-master/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+    private float spawn = 20;
+    public GameObject enemy;               
+
+    // Use this for initialization
+    void Start () {
+        Spawn();
+    }
+    private void Update()
+    {
+        if (spawn >= 0)
+        {
+            spawn = spawn - 1 * Time.deltaTime;
+        }
+        if(spawn <= 0)
+        {
+            Spawn();
+            spawn = 20;
+        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
---
+++ Labyrinth-vom-Laptop-master/Assets/Scripts/WallRotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRotation : MonoBehaviour {
+    private int turnx = 5;
+    public int collisionCount;
+    public float x;
+
+	void Start () {
+        collisionCount = 0;
+    }
+
+    void Update()
+    {
+        if (collisionCount == 0)
+        {
+            x = x - 1 * Time.deltaTime;
+        }
+
+        if(x <= 0)
+        {
+            int turnx = Random.Range(0, 10);
+            if (turnx == 2)
+            {
+                transform.Rotate(0, 90, 0);
+            }
+            else if (turnx == 1)
+            {
+                transform.Rotate(0, -90, 0);
+            }
+            else if (turnx == 4)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+            x = 5;
+        }
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        collisionCount++;
+    }
+
+
+    public void OnCollisionExit(Collision collision)
+    {
+        collisionCount--;
+    }
+}
